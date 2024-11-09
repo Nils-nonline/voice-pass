c38f431f59245f03296f72724f06dcd659b487ff
Nils <120112184+Nils-nonline@users.noreply.github.com>
2024-11-09 12:52:40 +0100

    Add files via upload

---
+++ server.py
@@ -0,0 +1,28 @@
+import eventlet
+from eventlet import wsgi
+import socketio
+
+hostName = "localhost"
+serverPort = 8080
+
+
+sio = socketio.Server()
+app = socketio.WSGIApp(sio, static_files={
+    '/': {'content_type': 'text/html', 'filename': 'index.html'}
+})
+
+@sio.event
+def connect(sid, environ):
+    print('connect ', sid)
+
+@sio.event
+def audio(sid, data):
+    print('audio', data)
+    sio.emit('newaudio',{'data': 'nodata!'})
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
+if __name__ == '__main__':
+    eventlet.wsgi.server(eventlet.listen((hostName, serverPort)), app)
