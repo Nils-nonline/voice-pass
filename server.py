b387212dd2041ae7d9701cc67a449ad9a1aae725
MHausOnline <118628201+MHausOnline@users.noreply.github.com>
2024-11-09 21:04:51 +0100

    Update server.py

--- server.py
+++ server.py
@@ -10,11 +10,10 @@ index = 0;
 
 @socketio.on('audio')
 def handle_audio(data):
-    print(data["d"])
-    audio_path = os.path.join(UPLOAD_FOLDER, 'audio'+str(data["index"])+'.wav')
-    with open(audio_path, 'ab') as f:
-        f.write(data["d"])
-	index = data["index"]
-
+	print(data["d"])
+	audio_path = os.path.join(UPLOAD_FOLDER, 'audio'+str(data["index"])+'.wav')
+	with open(audio_path, 'ab') as f:
+		f.write(data["d"])
+		index = data["index"]
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=1337)
+	socketio.run(app, host='0.0.0.0', port=1337)
