b063e7ddec9cd66f66b096156bec4a4781f662b0
MHausOnline <118628201+MHausOnline@users.noreply.github.com>
2024-11-10 11:37:16 +0100

    Add files via upload

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_socketio import SocketIO
 import os
 import random
+import json
 from random_noise import Random as ran
 import FFT_Backend as fft
 app = Flask(__name__)
@@ -11,22 +12,28 @@ for i in range(0,11):
 	ran(44100,72000,index=i)
 print("...starting server")
 def confirm(audio,passw):
-	result = random.randint(0,3)==1#fft.check(audio,passw)
-	os.remove(audio)
-	return result
+	fft.Vergleich(passw,audio)
+	return True
 
 @socketio.on('audio')
 def handle_audio(data):
 	index = data["index"]
-	f = open("{}audio.wav".format(index),"wb")
+	f = open("audio.wav","wb")
 	f.write(data["d"])
 	f.close()
-	if confirm(str(index)+"audio.wav",'pass.wav'):
+	if confirm("audio.wav",'pass.wav'):
 		socketio.emit("valid",{})
-		f = open('urls.json',)
-		locked_urls = json.load(f)["locked"].remove(data["hash"])
-		json.dump(locked_urls,f)
-		f.close()
+		try:
+			f = open('urls.json',)
+			locked_urls = json.load(f)["locked"]
+			remove_index = locked_urls.index(data["hash"])
+			locked_urls.pop(remove_index)
+			print(locked_urls)
+			json.dump(locked_urls,f)
+			f.close()
+		except:
+			pass
+
 
 @socketio.on('pass')
 def handle_pass(data):
